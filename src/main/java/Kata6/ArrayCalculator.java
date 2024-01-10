70fe1f39ff019d34b0ebf00039181b5425d0d6d2
Mario <mario2002pc@hotmail.com>
2024-01-10 23:00:55 +0000

    Creation of tests and develop the code

--- src/main/java/Kata6/ArrayCalculator.java
+++ src/main/java/Kata6/ArrayCalculator.java
@@ -1,4 +1,50 @@
 package Kata6;
 
 public class ArrayCalculator {
+    public int Take_out_the_largest(int[] Numbers) {
+        if (Numbers.length == 0) {
+            return -102;
+        } else if (Numbers.length == 1) {
+            return Numbers[0];
+        } else {
+            int x = Numbers[0];
+            for (int i = 1; i < Numbers.length; i++) {
+                if (x < Numbers[i]) {
+                    x = Numbers[i];
+                }
+            }
+            return x;
+        }
+    }
+
+    public int Take_out_the_minimum(int[] Numbers) {
+        if (Numbers.length == 0) {
+            return -102;
+        } else if (Numbers.length == 1) {
+            return Numbers[0];
+        } else {
+            int x = Numbers[0];
+            for (int i = 1; i < Numbers.length; i++) {
+                if (x > Numbers[i]) {
+                    x = Numbers[i];
+                }
+            }
+            return x;
+        }
+    }
+
+    public int Take_out_the_middle_one(int[] Numbers) {
+        if (Numbers.length == 0) {
+            return -102;
+        } else if (Numbers.length == 1) {
+            return Numbers[0];
+        } else {
+            if (Numbers.length % 2 == 0) {
+                int X = Numbers[(Numbers.length/2) - 1];
+                return X + Numbers[(Numbers.length/2)];
+            } else {
+                return Numbers[(Numbers.length/2)];
+            }
+        }
+    }
 }
--- src/test/java/tests/ArrayCalculatorTest.java
+++ src/test/java/tests/ArrayCalculatorTest.java
@@ -1,4 +1,77 @@
 package tests;
 
+import Kata6.ArrayCalculator;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ArrayCalculatorTest {
+    // Vamos a utilizar 102 como valor de error
+
+    @Test
+    public void should_be_50() {
+        int[] Numeros = {10, 42, 31, -10, 50, 40, 26};
+        ArrayCalculator ac = new ArrayCalculator();
+        assertThat(ac.Take_out_the_largest(Numeros)).isEqualTo(50);
+    }
+    @Test
+    public void should_be_negative() {
+        int[] Numeros = {-10, -42, -31, -10, -50, -40, -26};
+        ArrayCalculator ac = new ArrayCalculator();
+        assertThat(ac.Take_out_the_largest(Numeros)).isEqualTo(-10);
+    }
+    @Test
+    public void should_be_negative_102() {
+        int[] Numeros = {};
+        ArrayCalculator ac = new ArrayCalculator();
+        assertThat(ac.Take_out_the_largest(Numeros)).isEqualTo(-102);
+    }
+    @Test
+    public void should_be_5() {
+        int[] Numeros = {5};
+        ArrayCalculator ac = new ArrayCalculator();
+        assertThat(ac.Take_out_the_largest(Numeros)).isEqualTo(5);
+    }
+    @Test
+    public void should_be_4() {
+        int[] Numeros = {4, 10, 60, 80 , 4, 90};
+        ArrayCalculator ac = new ArrayCalculator();
+        assertThat(ac.Take_out_the_minimum(Numeros)).isEqualTo(4);
+    }
+    @Test
+    public void should_be_negative_102_2() {
+        int[] Numeros = {};
+        ArrayCalculator ac = new ArrayCalculator();
+        assertThat(ac.Take_out_the_minimum(Numeros)).isEqualTo(-102);
+    }
+    @Test
+    public void should_be_5_2() {
+        int[] Numeros = {5};
+        ArrayCalculator ac = new ArrayCalculator();
+        assertThat(ac.Take_out_the_minimum(Numeros)).isEqualTo(5);
+    }
+    @Test
+    public void should_be_the_middle_one() {
+        int[] Numeros = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+        ArrayCalculator ac = new ArrayCalculator();
+        assertThat(ac.Take_out_the_middle_one(Numeros)).isEqualTo(50);
+    }
+    @Test
+    public void should_be_the_middle_one_2() {
+        int[] Numeros = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        ArrayCalculator ac = new ArrayCalculator();
+        assertThat(ac.Take_out_the_middle_one(Numeros)).isEqualTo(110);
+    }
+
+    @Test
+    public void should_be_negative_102_3() {
+        int[] Numeros = {};
+        ArrayCalculator ac = new ArrayCalculator();
+        assertThat(ac.Take_out_the_middle_one(Numeros)).isEqualTo(-102);
+    }
+    @Test
+    public void should_be_5_3() {
+        int[] Numeros = {5};
+        ArrayCalculator ac = new ArrayCalculator();
+        assertThat(ac.Take_out_the_middle_one(Numeros)).isEqualTo(5);
+    }
 }
